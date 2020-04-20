1c7c69113f39b01a224f0a5e2c908d473cf0a774
makewebme <makewebme@gmail.com>
2020-04-20 13:06:21 +0300

    Lesson 3 Commits

--- gulpfile.js
+++ gulpfile.js
@@ -18,10 +18,8 @@ function css() {
   return src('scss/style.scss')
     .pipe(scss())
     // .pipe(minifyCSS())
+    .pipe(autoprefixer({ cascade: false }))
     .pipe(dest('build/css'))
-    .pipe(autoprefixer({
-      cascade: false
-    }))
 }
 
 // function js() {
@@ -30,9 +28,19 @@ function css() {
 //     .pipe(dest('build/js', { sourcemaps: true }))
 // }
 
+function copyImg() {
+  return src('img/**/*.{jpg,jpeg,gif,png,svg}').pipe(dest('build/img'))
+}
+
+function copyFonts() {
+  return src('fonts/*.*').pipe(dest('build/fonts'))
+}
+
 task('watcher', _=> {
   watch('pug/**/*.pug', series(html))
   watch('scss/**/*.scss', series(css))
+  watch('img/**/*.{jpg,jpeg,gif,png,svg}', series(copyImg))
+  watch('fonts/*.*', series(copyFonts))
 })
 
 task('serve', _ => {
@@ -41,12 +49,14 @@ task('serve', _ => {
           baseDir: 'build'
       },
       port: 1234,
-      notify: true
-  });
+      notify: true,
+      open: false
+  })
+
   bs.watch('build/**/*.*').on('change', bs.reload)
-});
+})
 
 exports.default = parallel(
-  series(html, css, 'watcher'),
+  series(html, css, copyImg, copyFonts, 'watcher'),
   'serve'
-  )
+)
