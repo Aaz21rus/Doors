ca70fb050755508182b894098ed9ae97285e5b61
Dmitriy <Aaz21rus@mail.ru>
2020-05-09 18:37:00 +0300

    js

--- js/minjs.js
+++ js/minjs.js
@@ -1,4 +1,19 @@
-const btnLevel1 = querySelectorAll('.doors-name');
+var openAsideMenu = function(targetLi,item) {
+  item.addEventListener('click', function(e){
+    e.currentTarget.parentElement.classList.toggle('active')
+    document.querySelectorAll(targetLi + '> li')
+      .forEach(function(li) {
+          if (li === e.currentTarget.parentElement) return
+          li.classList.remove('active')
+        }
+      )
+  })
+};
 
-console.log(btnLevel1);
+var btnLevel1 = document.querySelectorAll('.doors-name')
+btnLevel1.forEach(openAsideMenu.bind(this, '.doors'))
+
+var btnLevel2 = document.querySelectorAll('.door-material')
+btnLevel2.forEach(openAsideMenu.bind(this, '.door-mat-list'))
+
 
