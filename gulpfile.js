1c2dd6715a7530f37ade3b6bbad5806f5d8b1b64
makewebme <makewebme@gmail.com>
2020-05-11 12:16:48 +0300

    Scripts build refactoring (added webpack)

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const { src, dest, parallel, watch, task, series } = require('gulp')
+const path = require('path')
 const pug = require('gulp-pug')
 const scss = require('gulp-sass')
+const gulplog = require('gulplog')
+const notifier = require('node-notifier')
 const bs = require('browser-sync')
 const autoprefixer = require('gulp-autoprefixer')
 const plumber = require('gulp-plumber')
 const notify = require('gulp-notify')
 // const minifyCSS = require('gulp-csso')
 const concat = require('gulp-concat')
+const webpack = require('webpack')
 
 function html() {
   return src('pug/pages/*.pug')
@@ -46,6 +50,58 @@ function js() {
     .pipe(dest('build/js'))
 }
 
+task('webpack', function(callback) {
+
+  let options = {
+    entry: {
+      scripts: './js/index',
+    },
+    output: {
+      path: __dirname + '/build/js',
+      filename: '[name].js'
+    },
+    watch: true,
+    mode: 'development',
+    devtool: 'cheap-module-inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: path.join(__dirname, 'js'),
+          loader: 'babel-loader?presets[]=@babel/env'
+        }
+      ]
+    }
+  }
+
+  webpack(options, function(err, stats) {
+    if (!err) { // no hard error
+      // try to get a soft error from stats
+      err = stats.toJson().errors[0];
+    }
+
+    if (err) {
+      notifier.notify({
+        title: 'Webpack',
+        message: err
+      })
+
+      gulplog.error(err)
+    } else {
+      gulplog.info(stats.toString({
+        colors: true
+      }))
+    }
+
+    // task never errs in watch mode, it waits and recompiles
+    if (!options.watch && err) {
+      callback(err)
+    } else {
+      callback()
+    }
+  })
+})
+
 function copyImg() {
   return src('img/**/*.{jpg,jpeg,gif,png,svg}').pipe(dest('build/img'))
 }
@@ -59,7 +115,7 @@ task('watcher', _=> {
   watch('scss/**/*.scss', series(css))
   watch('img/**/*.{jpg,jpeg,gif,png,svg}', series(copyImg))
   watch('fonts/*.*', series(copyFonts))
-  watch('js/**/*.js', series(js))
+  // watch('js/**/*.js', series(js))
 })
 
 task('serve', _ => {
@@ -76,6 +132,6 @@ task('serve', _ => {
 })
 
 exports.default = parallel(
-  series(html, css, copyImg, copyFonts, js, 'watcher'),
+  series(html, css, 'webpack', copyImg, copyFonts, 'watcher'),
   'serve'
 )
